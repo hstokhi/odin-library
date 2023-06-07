37014b90d6d64e8cd9acb4c8341e97f91ea0c6ff
Hakim Tokhi <hstokhi@yahoo.com>
2023-06-07 14:00:55 -0700

    Implement form to add book to library
    Added Pop up form via HTML that prompts user for
    book information. Associated Javascript code adds book
    to library upon clicking submit button.
    
    Also, started working on read/unread and remove from library
    buttons, but having some issues getting them to work.

--- script.js
+++ script.js
@@ -16,13 +16,6 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary(title, author, pages, read) {
-    /*const title = prompt('Title:')
-    const author = prompt('Author:')
-    const pages = parseInt(prompt('Number of pages:'))
-    if (prompt("Read? (Yes or No):").toLowerCase() == "yes") {
-        let read = true
-    } else {let read = false}*/
-
     library.push(new Book(title, author, pages, read))
 }
 
@@ -41,9 +34,10 @@ function displayLibrary(library) {
         div.appendChild(button)
         div.appendChild(removeButton)
         button.textContent = 'Read/Unread'
+        button.classList.add('read-unread')
         removeButton.textContent = 'Remove from Library'
-        
-
+        removeButton.classList.add('remove-button')
+        removeButton.dataset.arrayIndex = i
     }    
 }
 
@@ -53,5 +47,48 @@ function clearBox(container) {
     }
 }
 
+function openForm() {
+    document.querySelector('.form-container').style.display = 'block'
+}
+
+function closeForm() {
+    document.querySelector('.form-container').style.display = 'none'
+}
+
+document.querySelector('.header>button').addEventListener('click', (event) => {
+    if (document.querySelector('.form-container').style.display === 'none') {
+        openForm()
+    } else {closeForm()}
+})
+
+function submitForm() {
+    if (document.querySelector('input[name="read"]:checked').value == 'yes') {
+        addBookToLibrary(document.getElementById('title').value, document.getElementById('author').value, document.getElementById('pages').value, true)} 
+        else {addBookToLibrary(document.getElementById('title').value, document.getElementById('author').value, document.getElementById('pages').value, false)}
+    displayLibrary(library)
+}
+
+document.getElementById('submit-form').addEventListener('click', (event) => {
+    event.preventDefault()
+    submitForm()
+    closeForm()
+})
+
+function removeFromLibrary(index) {
+    library.splice(index, 1)
+    displayLibrary(library)
+}
+document.querySelectorAll('.remove-button').forEach( (button) => {
+    button.addEventListener('click', removeFromLibrary(button.dataset.arrayIndex))
+})
+
+document.querySelectorAll('.read-unread').forEach( (button) => {
+    button.addEventListener('click', (event) => {
+        let index = button.parentNode.dataset.arrayIndex
+        if (library[index].read == false) {library[index].read = true} else { library[index].read = false}
+        displayLibrary(library)
+    })
+})
 
 displayLibrary(library)
+closeForm()
